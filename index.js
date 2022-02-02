84ff8011da4f32d91d29263e9042a983fb68026d
Tanaka I Kahwai <iankahwai@gmail.com>
2022-02-03 00:57:26 +0200

    Update contract for accounting

--- index.js
+++ index.js
@@ -6,7 +6,9 @@
 
 'use strict';
 
-const FabCar = require('./lib/fabcar');
 
-module.exports.FabCar = FabCar;
-module.exports.contracts = [ FabCar ];
+const Account = require('./lib/account');
+
+module.exports.Account = Account
+
+module.exports.contracts = [ Account ];
